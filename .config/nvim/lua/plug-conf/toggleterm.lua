912ea1ce828753ac2cc67cc969e79ac4b70d6303
Ricky Lopez <rickyerlopez@gmail.com>
2023-01-19 01:13:40 +0000

    Various updates
    - Replace airline with lualine
    - Add lazygit configs
    - Update toggleterm binds
    - small updates to lspconfig, theme, neo-tree

--- .config/nvim/lua/plug-conf/toggleterm.lua
+++ .config/nvim/lua/plug-conf/toggleterm.lua
@@ -13,8 +13,10 @@ local lazygit = Terminal:new({
     hidden = true,
     direction = "float",
     on_open = function(term)
-        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-        vim.api.nvim_buf_set_keymap(term.bufnr, 't', 'q', [[<C-\><C-n>]], { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(term.bufnr, "t", "q", [[<nop>]], { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(term.bufnr, "t", [[<C-\>]], [[<C-\><C-n><cmd>close<CR>]], { noremap = true, silent = true })
+        -- vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+        -- vim.api.nvim_buf_set_keymap(term.bufnr, 't', 'q', [[<C-\><C-n>]], { noremap = true, silent = true })
     end,
 })
 
@@ -23,10 +25,10 @@ function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
   -- vim.keymap.set('t', 'q', [[<C-\><C-n>]], opts)
   -- vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  -- vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  -- vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  -- vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  -- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
 end
 
 -- use term://*toggleterm#* to apply to only toggleterm terminals
