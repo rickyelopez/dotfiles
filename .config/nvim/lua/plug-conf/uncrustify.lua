11ee5f54558dcdf851e84d89ccfef5e400e156d2
Ricky Lopez <rickyerlopez@gmail.com>
2023-03-31 21:42:56 +0000

    fix uncrustify lua config

--- .config/nvim/lua/plug-conf/uncrustify.lua
+++ .config/nvim/lua/plug-conf/uncrustify.lua
@@ -1,11 +1,11 @@
 local map = require("utils").map
 
-vim.g.uncrustify_config_file = "$HOME/.config/uncrustify.cfg"
+vim.g.uncrustify_config_file = vim.fn.expand("$HOME") .. "/.config/uncrustify.cfg"
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = {"cpp", "c"},
+    pattern = { "cpp", "c" },
     callback = function(ev)
-        map("<leader>f", "<CMD>Uncrustify<CR>", "n", {noremap = true, silent = true, buffer = ev.buf})
-        map("<leader>f", "<CMD>UncrustifyRange<CR>", "v", {noremap = true, silent = true, buffer = ev.buf})
+        map("<leader>f", ":Uncrustify<CR>", "n", { noremap = true, silent = true, buffer = ev.buf })
+        map("<leader>f", ":UncrustifyRange<CR>", "v", { noremap = true, silent = true, buffer = ev.buf })
     end,
 })
