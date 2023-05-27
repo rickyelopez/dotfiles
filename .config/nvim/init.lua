df22c9d3afe9cb04b30999d394794a3158d415e4
Ricky Lopez <rickyerlopez@gmail.com>
2023-05-27 09:41:34 -0700

    hyprland updates, add neovide

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -89,6 +89,16 @@ vim.g.clipboard = {
 -- source leftover .vim file that I don't feel like converting
 vim.api.nvim_exec([[ source $HOME/.config/nvim/plug-conf/fzf.vim ]], false)
 
+
+-- neovide settings
+if vim.g.neovide then
+    vim.g.neovide_scale_factor = 0.5
+    vim.g.neovide_floating_blur_amount_x = 2.0
+    vim.g.neovide_floating_blur_amount_y = 2.0
+    vim.g.neovide_transparency = 0.8
+    vim.g.neovide_cursor_vfx_mode = "torpedo"
+end
+
 -- plugins
 require("plug-conf")
 
