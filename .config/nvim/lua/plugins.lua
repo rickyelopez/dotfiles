46b7e2451ed5069e260b0723dd1867d633ab5173
Ricky Lopez <rickyerlopez@gmail.com>
2023-10-25 16:54:55 -0700

    switch to lazy.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,145 +1,119 @@
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-        vim.cmd([[packadd packer.nvim]])
-        return true
-    end
-    return false
+--- Plugin manager configuration
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
-
-local packer_bootstrap = ensure_packer()
-
-return require("packer").startup({
-    function(use)
-        use("wbthomason/packer.nvim")
-        use("lewis6991/impatient.nvim")
-
-        use('ojroques/nvim-osc52')
-
-        use("williamboman/mason.nvim")
-        use("williamboman/mason-lspconfig.nvim")
-        use("neovim/nvim-lspconfig")
-
-        use("p00f/clangd_extensions.nvim")
-
-        use("jose-elias-alvarez/null-ls.nvim")
-
-        -- completion
-        use("hrsh7th/cmp-nvim-lsp")
-        use("hrsh7th/cmp-buffer")
-        use("hrsh7th/cmp-path")
-        use("hrsh7th/cmp-cmdline")
-        use("hrsh7th/cmp-vsnip")
-        use("hrsh7th/nvim-cmp")
-
-        -- snippets
-        use("hrsh7th/vim-vsnip")
-        use("hrsh7th/vim-vsnip-integ")
-
-        -- treesitter
-        use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-        use("nvim-treesitter/playground")
-        use("nvim-treesitter/nvim-treesitter-context")
-
-        -- tpope plugins
-        use("tpope/vim-commentary")
-        use("tpope/vim-fugitive")
-        use("tpope/vim-surround")
-        use("tpope/vim-dispatch")
-
-        -- git plugins
-        use("sindrets/diffview.nvim")
-        use("APZelos/blamer.nvim")
-        use("lewis6991/gitsigns.nvim")
-
-        -- Merge conflicts
-        use("rhysd/conflict-marker.vim")
-
-        use({
-            "junegunn/fzf.vim",
-            requires = { "junegunn/fzf", run = ":call fzf#install()" },
-        })
-
-        use("airblade/vim-rooter")
-
-        use("vim-utils/vim-man")
-        use("mbbill/undotree")
-
-        use("jiangmiao/auto-pairs")
-
-        -- python
-        use({ "numirias/semshi", run = ":UpdateRemotePlugins" })
-
-        -- lualine
-        use("RRethy/nvim-base16")
-        use("nvim-lualine/lualine.nvim")
-
-        -- formatting
-        -- use("rickyelopez/vim-uncrustify")
-        use("rickyelopez/uncrustify.nvim")
-
-        -- color schemes
-        use("chriskempson/base16-vim")
-        use("norcalli/nvim-colorizer.lua")
-        use("folke/tokyonight.nvim")
-        use("rebelot/kanagawa.nvim")
+vim.opt.rtp:prepend(lazypath)
 
 
-        -- indent guides
-        use("lukas-reineke/indent-blankline.nvim")
+require("lazy").setup({
+    -- "wbthomason/packer.nvim",
+    -- "lewis6991/impatient.nvim",
 
-        -- Buffer line
-        use("kyazdani42/nvim-web-devicons")
-        use("akinsho/nvim-bufferline.lua")
-
-        -- toggleterm
-        use({ "akinsho/toggleterm.nvim", tag = "*" })
-        use("da-moon/telescope-toggleterm.nvim")
-
-        -- LSP Trouble
-        -- use("folke/lsp-trouble.nvim")
-
-        -- this needs to be fixed if I want it
-        -- use 'iamcco/markdown-preview.nvim', { 'do': { -> mkdp#util#install() }, 'for': ['markdown', 'vim-plug']}
-
-        -- telescope
-        use("nvim-lua/popup.nvim")
-        use("nvim-lua/plenary.nvim")
-        use("nvim-telescope/telescope.nvim")
-        use("nvim-telescope/telescope-fzy-native.nvim")
-        use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-
-        -- file browser
-        use("MunifTanjim/nui.nvim")
-        use("nvim-neo-tree/neo-tree.nvim")
-
-        -- CSV handling
-        use("chrisbra/csv.vim")
-
-        -- mako handling
-        use("sophacles/vim-bundle-mako")
-
-        -- Rust
-        use("rust-lang/rust.vim")
-
-        -- plugins from dotfiles_priv
-        local ok, privPlugins = pcall(require, "lua-priv.plugins")
-        if ok and privPlugins then
-            use(privPlugins)
-        end
-
-        -- Automatically set up your configuration after cloning packer.nvim
-        -- Put this at the end after all plugins
-        if packer_bootstrap then
-            require("packer").sync()
-        end
-    end,
-    config = {
-        profile = {
-            enable = true,
-            threshold = 0.2,
-        },
+    -- color schemes
+    {
+        "folke/tokyonight.nvim",
+        priority = 1000 ,
+        dependencies = {
+            "chriskempson/base16-vim",
+            "norcalli/nvim-colorizer.lua",
+        }
     },
+    -- "rebelot/kanagawa.nvim",
+
+    {
+        "williamboman/mason-lspconfig.nvim", dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" }
+    },
+
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            -- snippets
+            "hrsh7th/vim-vsnip",
+            "hrsh7th/vim-vsnip-integ",
+            -- completion
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-vsnip",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+        }
+    },
+
+    -- treesitter
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "nvim-treesitter/playground", dependencies = "nvim-treesitter/nvim-treesitter" },
+    { "nvim-treesitter/nvim-treesitter-context", dependencies = "nvim-treesitter/nvim-treesitter" },
+
+    -- git plugins
+    { "sindrets/diffview.nvim", dependencies = {"kyazdani42/nvim-web-devicons"} },
+    "APZelos/blamer.nvim",
+    "lewis6991/gitsigns.nvim",
+    "rhysd/conflict-marker.vim",
+
+    {
+        "junegunn/fzf.vim",
+        dependencies = { "junegunn/fzf", build = ":call fzf#install()" },
+    },
+
+    -- python
+    { "numirias/semshi", build = ":UpdateRemotePlugins" },
+
+
+    -- toggleterm
+    { "akinsho/toggleterm.nvim", version = "*" },
+    { "da-moon/telescope-toggleterm.nvim", dependencies = "akinsho/toggleterm.nvim" },
+
+    {
+        "iamcco/markdown-preview.nvim",
+        enabled = false,
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
+
+    -- "nvim-lua/popup.nvim",
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-fzy-native.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        }
+    },
+
+    -- misc
+    { "akinsho/nvim-bufferline.lua", dependencies = { "kyazdani42/nvim-web-devicons" } },
+    { "nvim-lualine/lualine.nvim", dependencies = { "RRethy/nvim-base16" } },
+    { "nvim-neo-tree/neo-tree.nvim", dependencies = { "MunifTanjim/nui.nvim" } },
+    { "jiangmiao/auto-pairs", enabled = false },
+    { "tpope/vim-fugitive", enabled = false },
+    { "tpope/vim-dispatch", enabled = false },
+    "tpope/vim-commentary",
+    "tpope/vim-surround",
+    "ojroques/nvim-osc52",
+    "airblade/vim-rooter",
+    "mbbill/undotree",
+    "lukas-reineke/indent-blankline.nvim",
+    "chrisbra/csv.vim",
+    "sophacles/vim-bundle-mako",
+    "rust-lang/rust.vim",
+    "p00f/clangd_extensions.nvim",
+    "jose-elias-alvarez/null-ls.nvim",
+    -- "rickyelopez/vim-uncrustify",
+    "rickyelopez/uncrustify.nvim",
+
+    -- -- plugins from dotfiles_priv
+    -- local ok, privPlugins = pcall(require, "lua-priv.plugins")
+    -- if ok and privPlugins then
+    --     use(privPlugins)
+    -- end
 })
