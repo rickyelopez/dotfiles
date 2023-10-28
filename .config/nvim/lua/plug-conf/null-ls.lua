3aea5ed48a32b06f58cf63bb41020a936a174aff
Ricky Lopez <rickyerlopez@gmail.com>
2023-10-27 17:49:45 -0700

    implement plug conf in lazy config and nvim-tree
    - instead of calling everything from plug-conf/init.lua, put all the setup stuff in the respective plug-conf.xxx file
    some general cleanup too
    - switch to nvim-tree

--- .config/nvim/lua/plug-conf/null-ls.lua
+++ .config/nvim/lua/plug-conf/null-ls.lua
@@ -1,14 +1,19 @@
-local null_ls = require("null-ls")
-local formatting = null_ls.builtins.formatting
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    local formatting = null_ls.builtins.formatting
 
-local black_config = vim.env.BLACK_CFG and { "--config", vim.env.BLACK_CFG } or {}
+    local black_config = vim.env.BLACK_CFG and { "--config", vim.env.BLACK_CFG } or {}
 
-null_ls.setup({
-  debug = true,
-  sources = {
-    formatting.stylua,
-    formatting.black.with(black_config),
-    formatting.yamlfmt,
-    formatting.rustfmt,
-  },
-})
+    null_ls.setup({
+      debug = true,
+      sources = {
+        formatting.stylua,
+        formatting.black.with(black_config),
+        formatting.yamlfmt,
+        formatting.rustfmt,
+      },
+    })
+  end,
+}
