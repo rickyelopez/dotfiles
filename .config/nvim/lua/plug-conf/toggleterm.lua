ed8f0feb6821925ee4622aea8e6289a99bc2f698
Ricky Lopez <rickyerlopez@gmail.com>
2024-07-18 12:47:12 -0700

    minor changes to lazygit and toggleterm
    lazygit:
      - unbind key to stage all, I never use it and it causes lazygit to
    hang if you hit it by accident with a lot of changes
      - remove some unused branches from the fetch selector
    toggleterm:
      - bind <Esc> to exit insert mode when in a terminal
      - remove some duplicated code

--- .config/nvim/lua/plug-conf/toggleterm.lua
+++ .config/nvim/lua/plug-conf/toggleterm.lua
@@ -1,7 +1,6 @@
 return {
   {
     "akinsho/toggleterm.nvim",
-    version = "*",
     config = function()
       require("toggleterm").setup({
         open_mapping = [[<c-\>]],
@@ -15,43 +14,47 @@ return {
 
       local Terminal = require("toggleterm.terminal").Terminal
 
+      --- Keymaps that are common to TUI apps
+      local function set_tui_keymaps(term)
+        vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<Esc>", "<Esc>", { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(term.bufnr, "t", "q", "q", { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(term.bufnr, "t", [[<C-\>]], [[<cmd>close<CR>]], { noremap = true, silent = true })
+      end
+
       local lazygit = Terminal:new({
+        display_name = "lazygit",
         cmd = "lazygit",
         count = 10,
         hidden = true,
         direction = "float",
-        on_open = function(term)
-          vim.api.nvim_buf_set_keymap(term.bufnr, "t", "q", "q", { noremap = true, silent = true })
-          vim.api.nvim_buf_set_keymap(term.bufnr, "t", [[<C-\>]], [[<cmd>close<CR>]], { noremap = true, silent = true })
-        end,
+        on_open = set_tui_keymaps,
       })
 
       local lf = Terminal:new({
+        display_name = "File Explorer",
         cmd = "lf",
         count = 11,
         hidden = true,
         direction = "float",
-        on_open = function(term)
-          vim.api.nvim_buf_set_keymap(term.bufnr, "t", "q", "q", { noremap = true, silent = true })
-          vim.api.nvim_buf_set_keymap(term.bufnr, "t", [[<C-\>]], [[<cmd>close<CR>]], { noremap = true, silent = true })
-        end,
+        on_open = set_tui_keymaps,
       })
 
       local function set_terminal_keymaps()
-        -- local opts = {buffer = 0}
-        -- vim.keymap.set('t', 'q', [[<C-\><C-n>]], opts)
-        -- vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-        -- vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-        -- vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-        -- vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-        -- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+        local opts = { noremap = true, silent = true }
+        vim.api.nvim_buf_set_keymap(0, "t", "<Esc>", [[<Cmd>stopinsert<CR>]], opts)
+        -- vim.api.nvim_buf_set_keymap(0, "t", "<Esc>", [[<C-\><C-n>]], opts)
+        -- vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+        -- vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+        -- vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+        -- vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+        -- vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
       end
 
       -- use term://*toggleterm#* to apply to only toggleterm terminals
       -- use term://* for all terminals
       -- vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
       vim.api.nvim_create_autocmd({ "TermOpen" }, {
-        pattern = { "term::/*toggleterm#*" },
+        pattern = { "term://*toggleterm#*" },
         callback = set_terminal_keymaps,
       })
 
