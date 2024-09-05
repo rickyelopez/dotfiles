2bc5c3f3f65db8e240cb2bea247113aabf256c46
Ricky Lopez <rickyerlopez@gmail.com>
2024-09-05 11:28:47 -0700

    nix cleanup

---
+++ nix/modules/nix-core.nix
@@ -0,0 +1,22 @@
+{ pkgs, lib, ... }: {
+  nix = {
+    package = pkgs.nix;
+    useDaemon = true;
+    # settings = {
+    #   cores = 8;
+    # };
+
+    # do garbage collection weekly to keep disk usage low
+    gc = {
+      automatic = lib.mkDefault true;
+      options = lib.mkDefault "--delete-older-than 7d";
+    };
+
+    # Disable auto-optimise-store because of this issue:
+    #   https://github.com/NixOS/nix/issues/7273
+    # "error: cannot link '/nix/store/.tmp-link-xxxxx-xxxxx' to '/nix/store/.links/xxxx': File exists"
+    settings = {
+      auto-optimise-store = false;
+    };
+  };
+}
