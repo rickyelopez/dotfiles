c40008a95b733322128602b4a1140cfe3e0c626b
Ricky Lopez <rickyerlopez@gmail.com>
2024-12-04 14:02:26 -0800

    fix nil_ls and add git link nvim plugin

--- .config/nvim/lua/plug-conf/git.lua
+++ .config/nvim/lua/plug-conf/git.lua
@@ -2,6 +2,27 @@ return {
   "rhysd/conflict-marker.vim",
   { "sindrets/diffview.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
   {
+    "juacker/git-link.nvim",
+    keys = {
+      {
+        "<leader>gu",
+        function()
+          require("git-link.main").copy_line_url()
+        end,
+        desc = "Copy code link to clipboard",
+        mode = { "n", "x" },
+      },
+      {
+        "<leader>go",
+        function()
+          require("git-link.main").open_line_url()
+        end,
+        desc = "Open code link in browser",
+        mode = { "n", "x" },
+      },
+    },
+  },
+  {
     "lewis6991/gitsigns.nvim",
     config = function()
       local map = require("utils").map
