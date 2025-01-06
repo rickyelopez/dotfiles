8c74dfb3999df7634ea6172dd3a23540b8968304
Ricky Lopez <rickyerlopez@gmail.com>
2025-01-06 08:44:38 -0800

    move nix hyprland into module

--- nix/hosts/expedition/home.nix
+++ nix/hosts/expedition/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, home, config, lib, user, ... }:
+{ pkgs, home, config, user, ... }:
 {
   home = {
     packages = with pkgs; [
@@ -12,9 +12,6 @@
 
     file = let mkLink = config.lib.file.mkOutOfStoreSymlink; in {
       ".config/Kvantum".source = mkLink "${home}/dotfiles/.config/Kvantum";
-      ".config/hypr/hyprland.conf".source = mkLink "${home}/dotfiles/.config/hypr/hyprland.conf";
-      ".config/hypr/hyprlock.conf".source = mkLink "${home}/dotfiles/.config/hypr/hyprlock.conf";
-      ".config/hypr/scripts".source = mkLink "${home}/dotfiles/.config/hypr/scripts";
       ".config/qt5ct".source = mkLink "${home}/dotfiles/.config/qt5ct";
       ".config/qt6ct".source = mkLink "${home}/dotfiles/.config/qt6ct";
       ".config/rofi".source = mkLink "${home}/dotfiles/.config/rofi";
@@ -51,85 +48,8 @@
     };
   };
 
-  programs = {
-    hyprpanel = {
-      enable = true;
-      overlay.enable = true;
-      # systemd.enable = true;
-      settings = {
-        bar = {
-          network.label = false;
-          notifications.show_total = true;
-          customModules.cpuTemp.sensor = "/sys/class/hwmon/hwmon2/temp1_input";
-          customModules.netstat.networkInterface = "wlp0s20f3";
-        };
-        menus = {
-          clock.weather.enabled = false;
-          dashboard = {
-            powermenu.logout = "uwsm stop";
-            stats.enable_gpu = true;
-            shortcuts.enabled = false;
-            directories.enabled = false;
-          };
-          power.lowBatteryNotification = true;
-        };
-        theme = {
-          bar.outer_spacing = "0.2em";
-          font.size = "1.0rem";
-        };
-      };
-      layout = {
-        "bar.layouts" = {
-          "0" = {
-            "left" = [ "dashboard" "workspaces" "windowtitle" ];
-            "middle" = [ "media" ];
-            "right" = [
-              "volume"
-              "network"
-              "bluetooth"
-              "battery"
-              "cputemp"
-              "systray"
-              "clock"
-              "notifications"
-            ];
-          };
-          "1" = {
-            "left" = [ "dashboard" "workspaces" "windowtitle" ];
-            "middle" = [ "media" ];
-            "right" = [ "volume" "clock" "notifications" ];
-          };
-          "2" = {
-            "left" = [ "dashboard" "workspaces" "windowtitle" ];
-            "middle" = [ "media" ];
-            "right" = [ "volume" "clock" "notifications" ];
-          };
-        };
-      };
-    };
-  };
-
-  services = {
-    hyprpaper = {
-      enable = true;
-      package = inputs.hyprpaper.packages.${pkgs.system}.hyprpaper;
-      settings = {
-        preload =
-          [
-            "~/Nextcloud/Media/Wallpapers/1643793713226.jpg"
-            "~/Nextcloud/Media/Wallpapers/1694344616904955.jpg"
-            "~/Nextcloud/Media/Wallpapers/Dog Watcher.jpg"
-          ];
-
-        wallpaper = [
-          ",~/Nextcloud/Media/Wallpapers/1643793713226.jpg"
-        ];
-      };
-    };
-  };
-
   imports = [
-    inputs.hyprpanel.homeManagerModules.hyprpanel
     ../../home.nix
+    ../../modules/hyprland.nix
   ];
 }
