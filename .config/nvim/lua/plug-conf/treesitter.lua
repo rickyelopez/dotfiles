7935840cabc812439e1baca1bad786abdb73f751
Ricky Lopez <rickyerlopez@gmail.com>
2025-02-11 22:30:26 -0800

    misc treesitter and nix changes

--- .config/nvim/lua/plug-conf/treesitter.lua
+++ .config/nvim/lua/plug-conf/treesitter.lua
@@ -3,6 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
+      --- @diagnostic disable-next-line: missing-fields
       require("nvim-treesitter.configs").setup({
         sync_install = false,
         auto_install = true,
@@ -35,7 +36,7 @@ return {
           -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
           -- Using this option may slow down your editor, and you may see some duplicate highlights.
           -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
+          additional_vim_regex_highlighting = true -- {"python"},
         },
         playground = {
           enable = true,
@@ -56,12 +57,12 @@ return {
           },
         },
         indent = {
-          enable = false,
+          enable = true,
         },
       })
 
-      vim.opt.foldmethod = "expr"
-      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+      vim.wo.foldmethod = "expr"
+      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
     end,
   },
   {
