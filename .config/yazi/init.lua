bdb5dd6eee3057894e448aeac5d4588236232b4d
Ricky Lopez <rickyerlopez@gmail.com>
2025-02-11 22:28:41 -0800

    yazi: misc changes

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -11,3 +11,23 @@ Status:children_add(function()
     ui.Span(" "),
   })
 end, 500, Status.RIGHT)
+
+require("augment-command"):setup({
+    prompt = true,
+    default_item_group_for_prompt = "none",
+    smart_enter = true,
+    smart_paste = true,
+    smart_tab_create = false,
+    smart_tab_switch = false,
+    open_file_after_creation = false,
+    enter_directory_after_creation = false,
+    use_default_create_behaviour = false,
+    enter_archives = false,
+    extract_retries = 3,
+    recursively_extract_archives = false,
+    preserve_file_permissions = false,
+    must_have_hovered_item = true,
+    skip_single_subdirectory_on_enter = false,
+    skip_single_subdirectory_on_leave = false,
+    wraparound_file_navigation = true,
+})
