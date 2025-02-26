0e92d5200e92059cd5ec2acf7916f20a391a1b3d
Ricky Lopez <rickyerlopez@gmail.com>
2025-02-26 15:48:45 -0800

    yazi: set image_delay to 0, other misc

--- nix/modules/yazi.nix
+++ nix/modules/yazi.nix
@@ -3,6 +3,7 @@
     packages = with pkgs; [ yazi ];
 
     file = let mkLink = config.lib.file.mkOutOfStoreSymlink; in {
+      ".config/yazi/init.lua".source = mkLink "${home}/dotfiles/.config/yazi/init.lua";
       ".config/yazi/keymap.toml".source = mkLink "${home}/dotfiles/.config/yazi/keymap.toml";
       ".config/yazi/package.toml".source = mkLink "${home}/dotfiles/.config/yazi/package.toml";
       ".config/yazi/yazi.toml".source = mkLink "${home}/dotfiles/.config/yazi/yazi.toml";
