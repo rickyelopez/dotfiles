d039e4470e5a03a72b3c1f934beebb7dad765a37
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2025-03-17 13:31:37 -0700

    nvim: cleanup init.lua/utils.lua/server.lua
    add some ftplugin files to replace autocommands setting
    language-specific options

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -24,31 +24,50 @@ function M.listValidBuffers()
   return validBufs
 end
 
--- should probably port these to pure lua at some point
-vim.api.nvim_exec2(
-  [[
-  function! HighlightRepeats() range
-    let lineCounts = {}
-    let lineNum = a:firstline
-    while lineNum <= a:lastline
-      let lineText = getline(lineNum)
-      if lineText != ""
-        let lineCounts[lineText] = (has_key(lineCounts, lineText) ? lineCounts[lineText] : 0) + 1
-      endif
-      let lineNum = lineNum + 1
-    endwhile
-    exe 'syn clear Repeat'
-    for lineText in keys(lineCounts)
-      if lineCounts[lineText] >= 2
-        exe 'syn match Repeat "^' . escape(lineText, '".\^$*[]') . '$"'
-      endif
-    endfor
-  endfunction
+function M.setup()
+  -- enable dbc syntax highlighting
+  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { "*.dbc" },
+    command = "set filetype=dbc",
+  })
 
-  command! -range=% HighlightRepeats <line1>,<line2>call HighlightRepeats()
-  command! ClearRepeats :syn<space>clear<space>Repeat
-  ]],
-  {}
-)
+  -- use cpp comment style in c files and cpp files
+  vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = { "cpp", "c" },
+    command = [[set commentstring=//\ %s]],
+  })
+
+  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { "ZIP_BUILD" },
+    command = [[set ft=python]],
+  })
+
+  -- should probably port this to pure lua at some point
+  vim.api.nvim_exec2(
+    [[
+    function! HighlightRepeats() range
+      let lineCounts = {}
+      let lineNum = a:firstline
+      while lineNum <= a:lastline
+        let lineText = getline(lineNum)
+        if lineText != ""
+          let lineCounts[lineText] = (has_key(lineCounts, lineText) ? lineCounts[lineText] : 0) + 1
+        endif
+        let lineNum = lineNum + 1
+      endwhile
+      exe 'syn clear Repeat'
+      for lineText in keys(lineCounts)
+        if lineCounts[lineText] >= 2
+          exe 'syn match Repeat "^' . escape(lineText, '".\^$*[]') . '$"'
+        endif
+      endfor
+    endfunction
+
+    command! -range=% HighlightRepeats <line1>,<line2>call HighlightRepeats()
+    command! ClearRepeats :syn<space>clear<space>Repeat
+    ]],
+    {}
+  )
+end
 
 return M
