e784c094e500f0935db0d63549a4607408fe2945
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2025-04-11 21:41:25 -0700

    nix: make brave use gnome-keyring on linux

--- nix/platforms/linux/home/headed.nix
+++ nix/platforms/linux/home/headed.nix
@@ -13,7 +13,48 @@
       vlc
     ];
   };
+
   services = {
     ssh-agent.enable = true;
+  };
+
+  xdg.desktopEntries = {
+    brave-browser = {
+      name = "Brave Web Browser";
+      genericName = "Web Browser";
+      type = "Application";
+      exec = "${pkgs.brave}/bin/brave --password-store=gnome-libsecret %U";
+      terminal = false;
+      categories = [ "Network" "WebBrowser" ];
+      startupNotify = true;
+      icon = "brave-browser";
+      actions = {
+        "new-window" = {
+          name = "New Window";
+          exec = "${pkgs.brave}/bin/brave --password-store=gnome-libsecret";
+        };
+        "new-private-window" = {
+          name = "New Private Window";
+          exec = "${pkgs.brave}/bin/brave --password-store=gnome-libsecret --incognito";
+        };
+      };
+      mimeType = [
+        "text/html"
+        "text/xml"
+        "application/rdf+xml"
+        "application/rss+xml"
+        "application/xhtml+xml"
+        "application/xhtml_xml"
+        "application/xml"
+        "image/gif"
+        "image/jpeg"
+        "image/png"
+        "image/webp"
+        "text/html"
+        "text/xml"
+        "x-scheme-handler/http"
+        "x-scheme-handler/https"
+      ];
+    };
   };
 }
