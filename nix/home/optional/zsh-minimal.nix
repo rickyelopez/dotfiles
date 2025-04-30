601d1e8ac44b9ef8ad8b678d5f84309da4211f94
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2025-04-30 13:44:21 -0700

    nix: switch to starship, cleanup zsh.nix and zsh-minimal.nix

--- nix/home/optional/zsh-minimal.nix
+++ nix/home/optional/zsh-minimal.nix
@@ -13,16 +13,6 @@ in
     zsh = {
       enable = true;
 
-      antidote = {
-        enable = true;
-        plugins = [
-          "zsh-users/zsh-autosuggestions"
-          "zsh-users/zsh-syntax-highlighting"
-        ];
-      };
-
-      autosuggestion.enable = true;
-
       history = {
         append = true;
         save = 10000;
@@ -35,34 +25,14 @@ in
           export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock.$(hostname)
         fi
 
-        # use this cmd to open yazi and cd into whatever directory it is in when it exits
-        # taken from https://yazi-rs.github.io/docs/quick-start/
-        if command -v yazi &>/dev/null; then
-            ycd() {
-                local tmp
-                tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-
-                yazi "$@" --cwd-file="$tmp"
-
-                if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-                    builtin cd -- "$cwd" || return 1
-                fi
-
-                rm -f -- "$tmp"
-            }
-        fi
-
         # source private files
         [ -f "$HOME/dotfiles_priv/.privrc" ] && source "$HOME/dotfiles_priv/.privrc"
         [ -f "$HOME/dotfiles_priv/.vars" ] && source "$HOME/dotfiles_priv/.vars"
         [ -f "$HOME/dotfiles_priv/.aliases" ] && source "$HOME/dotfiles_priv/.aliases"
-
       '';
 
       localVariables = {
         DISABLE_UPDATE_PROMPT = "true";
-        COMPLETION_WAITING_DOTS = "true";
-        DISABLE_UNTRACKED_FILES_DIRTY = "true";
       };
 
       sessionVariables = {
@@ -82,6 +52,8 @@ in
           # "cursor"
         ];
       };
+
+      zprof.enable = false;
     };
   };
 }
--- nix/home/optional/zsh.nix
+++ nix/home/optional/zsh.nix
@@ -1,12 +1,15 @@
 { lib, hostSpec, ... }:
 let
   home = hostSpec.home;
-  user = hostSpec.username;
 in
 {
+  imports = [
+    ./starship.nix
+    ./zsh-minimal.nix
+  ];
+
   home = {
     sessionPath = [
-      "$HOME/.local/bin"
       "$HOME/.cargo/bin"
     ] ++ lib.optionals (hostSpec.isDarwin) [
       "$(/opt/homebrew/bin/brew --prefix)/opt/util-linux/bin"
@@ -21,34 +24,17 @@ in
 
   programs = {
     zsh = {
-      enable = true;
-
       antidote = {
         enable = true;
         plugins = [
-          "romkatv/powerlevel10k"
           "tinted-theming/tinted-shell"
-          "zsh-users/zsh-autosuggestions"
-          "zsh-users/zsh-syntax-highlighting"
         ];
       };
 
       autosuggestion.enable = true;
 
-      history = {
-        append = true;
-        save = 10000;
-        size = 10000;
-      };
-
       initExtra = /*bash*/ ''
-        [ -f ~/.p10k.zsh ] && source ~/.p10k.zsh
         [ -f "$HOME/.cargo/env" ] && source "$HOME/.cargo/env"
-
-        # make ssh work in tmux across reconnections
-        if [[ -n "$SSH_TTY" ]] && [[ -n "$SSH_AUTH_SOCK" ]]; then
-          export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock.$(hostname)
-        fi
 
         NVM_DIR="$HOME/.nvm"
         function load_nvm() {
@@ -74,74 +60,29 @@ in
             fi
         }
 
-        # use this cmd to open yazi and cd into whatever directory it is in when it exits
-        # taken from https://yazi-rs.github.io/docs/quick-start/
-        if command -v yazi &>/dev/null; then
-            ycd() {
-                local tmp
-                tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-
-                yazi "$@" --cwd-file="$tmp"
-
-                if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-                    builtin cd -- "$cwd" || return 1
-                fi
-
-                rm -f -- "$tmp"
-            }
-        fi
-
         # source private files
         [ -f "$HOME/dotfiles_priv/.privrc" ] && source "$HOME/dotfiles_priv/.privrc"
         [ -f "$HOME/dotfiles_priv/.vars" ] && source "$HOME/dotfiles_priv/.vars"
         [ -f "$HOME/dotfiles_priv/.aliases" ] && source "$HOME/dotfiles_priv/.aliases"
-
       '' + lib.optionalString (!hostSpec.isDarwin) "load_nvm";
 
-      # initExtraBeforeCompInit = '' '';
+      # initExtraBeforeCompInit = /*bash*/'' '';
 
-      initExtraFirst = /*bash*/ ''
-        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-        # Initialization code that may require console input (password prompts, [y/n]
-        # confirmations, etc.) must go above this block; everything else may go below.
-        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${user}.zsh" ]]; then
-          source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${user}.zsh"
-        fi
-      '';
+      # initExtraFirst = /*bash*/ '' '';
 
-      localVariables = {
-        DISABLE_UPDATE_PROMPT = "true";
-        COMPLETION_WAITING_DOTS = "true";
-        DISABLE_UNTRACKED_FILES_DIRTY = "true";
-      };
+      # localVariables = { };
 
       sessionVariables = {
-        XDG_CONFIG_HOME = "${home}/.config";
         UNCRUSTIFY_CONFIG = "${home}/.config/uncrustify.cfg";
       };
 
       # shellAliases = {};
-      shellGlobalAliases = {
-        G = "| grep";
-      };
-
-      syntaxHighlighting = {
-        enable = true;
-        highlighters = [
-          "main"
-          "brackets"
-          # "cursor"
-        ];
-      };
-
-      zprof.enable = false;
+      # shellGlobalAliases = { };
 
       oh-my-zsh = {
         enable = true;
-        extraConfig = /*bash*/ ''
-          zstyle ':omz:plugins:git' aliases no
-        '';
-        plugins = [ "git" "sudo" ];
+        extraConfig = /*bash*/ '' '';
+        plugins = [ "rust" ];
       };
     };
   };
