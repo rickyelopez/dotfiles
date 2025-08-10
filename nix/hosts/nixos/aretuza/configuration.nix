15e2693cdf95a39ccbe4477975c5a182277f3712
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2025-08-09 22:35:08 -0700

    aretuza: add windows to boot menu

--- nix/hosts/nixos/aretuza/configuration.nix
+++ nix/hosts/nixos/aretuza/configuration.nix
@@ -11,8 +11,28 @@
   ];
 
   boot.loader = {
-    systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
+    systemd-boot = {
+      enable = true;
+
+      windows = {
+        "windows" =
+          let
+            # To determine the name of the windows boot drive, boot into edk2 first, then run
+            # `map -c` to get drive aliases, and try out running `FS1:`, then `ls EFI` to check
+            # which alias corresponds to which EFI partition.
+            boot-drive = "FS4";
+          in
+          {
+            title = "Windows";
+            efiDeviceHandle = boot-drive;
+            sortKey = "y_windows";
+          };
+      };
+
+      edk2-uefi-shell.enable = true;
+      edk2-uefi-shell.sortKey = "z_edk2";
+    };
   };
 
   # format of the following described here: https://www.kernel.org/doc/Documentation/filesystems/nfs/nfsroot.txt
