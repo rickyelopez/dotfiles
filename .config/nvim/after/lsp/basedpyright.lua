ea7472151bd626ae3fee2b42a737a631361368e7
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2025-09-24 15:47:30 -0700

    nvim: set python root_markers manually

--- .config/nvim/after/lsp/basedpyright.lua
+++ .config/nvim/after/lsp/basedpyright.lua
@@ -1,10 +1,20 @@
 return {
+  root_markers = {
+    'pyproject.toml',
+    'setup.py',
+    'setup.cfg',
+    'requirements.txt',
+    'Pipfile',
+    'pyrightconfig.json',
+    '.git',
+  },
   settings = {
     basedpyright = {
       analysis = {
         autoSearchPaths = true,
         diagnosticMode = "openFilesOnly",
         useLibraryCodeForTypes = true,
+        useTypingExtensions = true,
       },
     },
   },
