de410c8042f3f0c64f269db70777a6e730689e12
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2025-10-15 14:31:22 -0700

    nix: update flake inputs and remove bitwarden-cli patch

--- nix/hosts/common/darwin.nix
+++ nix/hosts/common/darwin.nix
@@ -7,17 +7,6 @@
   ];
 
   nixpkgs.overlays = [
-    # issues building bitwarden-cli on darwin-aarch64
-    # https://github.com/NixOS/nixpkgs/issues/339576
-    (final: prev: {
-      bitwarden-cli = prev.bitwarden-cli.overrideAttrs (
-        finalAttrs: oldAttrs: {
-          nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ prev.llvmPackages_18.stdenv.cc ];
-          stdenv = prev.llvmPackages_18.stdenv;
-          meta.broken = false;
-        }
-      );
-    })
     outputs.overlays.default
   ];
 }
