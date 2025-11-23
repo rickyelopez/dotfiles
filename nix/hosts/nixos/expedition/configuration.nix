20c20869b1b6c1c5e14a636c616c34750e6fa202
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2025-11-23 13:17:51 -0800

    nix: change expedition hwmon to 3

--- nix/hosts/nixos/expedition/configuration.nix
+++ nix/hosts/nixos/expedition/configuration.nix
@@ -14,23 +14,23 @@ in
 
   hardware.fancontrol = {
     enable = true;
-    config = ''
-      DEVPATH=hwmon6=devices/platform/dell_smm_hwmon hwmon2=devices/platform/coretemp.0
+    config = /*ini*/''
+      DEVPATH=hwmon3=devices/platform/dell_smm_hwmon hwmon2=devices/platform/coretemp.0
 
-      DEVNAME=hwmon6=dell_smm hwmon2=coretemp
+      DEVNAME=hwmon3=dell_smm hwmon2=coretemp
 
-      FCTEMPS=hwmon6/pwm1=hwmon2/temp1_input hwmon6/pwm2=hwmon2/temp1_input
+      FCTEMPS=hwmon3/pwm1=hwmon2/temp1_input hwmon3/pwm2=hwmon2/temp1_input
 
-      FCFANS=hwmon6/pwm1=hwmon6/fan1_input hwmon6/pwm2=hwmon6/fan2_input
+      FCFANS=hwmon3/pwm1=hwmon3/fan1_input hwmon3/pwm2=hwmon3/fan2_input
 
       INTERVAL=1
-      AVERAGE=hwmon6/pwm2=15 hwmon6/pwm1=10
+      AVERAGE=hwmon3/pwm2=15 hwmon3/pwm1=10
 
       # the following values are in degC
       # MINTEMP: The temperature below which the fan gets switched to minimum speed
       # MAXTEMP: The temperature over which the fan gets switched to maximum speed
-      MINTEMP=hwmon6/pwm2=60 hwmon6/pwm1=60
-      MAXTEMP=hwmon6/pwm2=70 hwmon6/pwm1=70
+      MINTEMP=hwmon3/pwm2=60 hwmon3/pwm1=60
+      MAXTEMP=hwmon3/pwm2=70 hwmon3/pwm1=70
 
       # the following values are in PWM (0-255)
       # MINSTART: sets the minimum speed at which the fan begins spinning. You should use a safe value to be sure
@@ -39,10 +39,10 @@ in
       # MINPWM: The PWM value to use when the temperature is below MINTEMP. Typically, this will be either 0 if it
       #     is OK for the fan to plain stop, or the same value as MINSTOP if you don't want the fan to ever stop.
       #     If this value isn't defined, it defaults to 0 (stopped fan)
-      MINSTART=hwmon6/pwm2=64 hwmon6/pwm1=64
-      MINSTOP=hwmon6/pwm2=64 hwmon6/pwm1=64
-      MINPWM=hwmon6/pwm2=64 hwmon6/pwm1=64
-      MAXPWM=hwmon6/pwm2=192 hwmon6/pwm1=192
+      MINSTART=hwmon3/pwm2=64 hwmon3/pwm1=64
+      MINSTOP=hwmon3/pwm2=64 hwmon3/pwm1=64
+      MINPWM=hwmon3/pwm2=64 hwmon3/pwm1=64
+      MAXPWM=hwmon3/pwm2=192 hwmon3/pwm1=192
     '';
   };
 
