026d4c209e6bb54f0e3138165f9fac0f14fb4eea
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2025-11-25 18:02:09 -0800

    nix: add github token to sops

--- nix/modules/home-manager/sops.nix
+++ nix/modules/home-manager/sops.nix
@@ -18,6 +18,11 @@ in
 
   options.my.sops = {
     enable = lib.mkEnableOption "home sops-nix module.";
+    addKeys = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether or not to set up ssh private keys.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -27,25 +32,27 @@ in
       defaultSopsFile = lib.custom.relativeToRepoRoot "secrets.yaml";
       validateSopsFiles = false;
 
-      secrets = {
-        "private_keys/new" = {
-          path = "${home}/.ssh/id_new";
-        };
+      secrets = lib.mkIf cfg.addKeys (
+        {
+          "private_keys/new" = {
+            path = "${home}/.ssh/id_new";
+          };
 
-        "private_keys/old" = {
-          path = "${home}/.ssh/id_old";
-        };
-      }
-      // lib.optionalAttrs (hostSpec.isWork) {
-        "private_keys/zip" = {
-          path = "${home}/.ssh/id_zip";
-        };
-      }
-      // lib.optionalAttrs (!hostSpec.isWork) {
-        "private_keys/dg" = {
-          path = "${home}/.ssh/id_dg";
-        };
-      };
+          "private_keys/old" = {
+            path = "${home}/.ssh/id_old";
+          };
+        }
+        // lib.optionalAttrs (hostSpec.isWork) {
+          "private_keys/zip" = {
+            path = "${home}/.ssh/id_zip";
+          };
+        }
+        // lib.optionalAttrs (!hostSpec.isWork) {
+          "private_keys/dg" = {
+            path = "${home}/.ssh/id_dg";
+          };
+        }
+      );
     };
   };
 }
