564b02c17b371d3e39987b2b7e6835810a3f56a9
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2025-11-25 18:02:09 -0800

    nix: add github token to sops

--- nix/home/users/ricclopez/thinkrick.nix
+++ nix/home/users/ricclopez/thinkrick.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  ...
+}:
 {
   home = {
     packages = with pkgs; [
@@ -6,13 +10,25 @@
     ];
   };
 
+  programs.zsh.initContent = /* bash */ ''
+    export GITHUB_TOKEN="$(cat ${config.sops.secrets."github_tokens/work".path})"
+  '';
+
   imports = [
     ../../../platforms/linux/home
   ];
+
+  sops.secrets = {
+    "github_tokens/work" = { };
+  };
 
   my = {
     bazel.enable = true;
     docker.enable = true;
     remote-open.client.enable = true;
+    sops = {
+      enable = true;
+      addKeys = false;
+    };
   };
 }
