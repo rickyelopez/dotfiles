6e1fac13845ddf9421abc1d6cb882c9e73dfffc4
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2026-01-14 16:43:09 -0800

    nvim: use existing lspmux socket if possible

--- .config/nvim/after/lsp/rust_analyzer.lua
+++ .config/nvim/after/lsp/rust_analyzer.lua
@@ -1,7 +1,31 @@
+local cmd
+
+local socket_check = vim.system({ "bash", "-c", "ss -tuplen 2>/dev/null | grep 27631" }):wait()
+
+if socket_check.code == 0 then
+  cmd = vim.lsp.rpc.connect("127.0.0.1", 27631)
+else
+  local choice = vim.fn.confirm(
+    "Couldn't find an lspmux socket at localhots:27631. Do you want to start vanilla rust-analyzer??",
+    "&Yes\n&No",
+    2
+  )
+  if choice == 1 then
+    cmd = { "rust-analyzer" }
+  else
+    cmd = {}
+  end
+end
+
 return {
-  cmd = { vim.fn.executable("ra-multiplex") and "ra-multiplex" or "rust-analyzer" },
+  cmd = cmd,
   settings = {
     ["rust-analyzer"] = {
+      lspMux = {
+        version = "1",
+        method = "connect",
+        server = "rust-analyzer",
+      },
       files = {
         watcher = "server",
       },
