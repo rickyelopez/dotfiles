4f1d5731e9114f45c37acba15df1cb93c404f360
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2026-04-11 15:55:48 -0700

    nix: misc small changes

--- nix/home/users/nonroot/barry.nix
+++ nix/home/users/nonroot/barry.nix
@@ -1,8 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ../../../platforms/linux/home
   ];
+
+  home = {
+    packages = with pkgs; [
+      python314
+      python314Packages.pip
+      uv
+    ];
+  };
 
   my.docker.enable = true;
 }
