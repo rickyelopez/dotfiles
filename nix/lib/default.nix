2c72017b0faba78df3a973ecc0adfbe701266fb1
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2026-07-19 18:17:14 -0700

    nix: remove some builtins

--- nix/lib/default.nix
+++ nix/lib/default.nix
@@ -8,7 +8,7 @@
   # get all .nix files in the given dir
   scanPaths =
     path:
-    builtins.map (f: (path + "/${f}")) (
+    map (f: (path + "/${f}")) (
       builtins.attrNames (
         lib.attrsets.filterAttrs (
           path: typ:
