5e8513bb1ad926fb96039636e9f7ee944b2252e7
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2026-07-27 00:00:42 -0700

    nvim: add nixd options for completion

--- .config/nvim/after/lsp/nixd.lua
+++ .config/nvim/after/lsp/nixd.lua
@@ -7,6 +7,19 @@ return {
       formatting = {
         command = { "nixfmt" },
       },
+      nixpkgs = {
+        expr = "import <nixpkgs> { }",
+      },
+      options = {
+        nixos = {
+          expr = vim.fn.expand('(builtins.getFlake ("git+file://$HOME/dotfiles")).nixosConfigurations.$HOST.options'),
+        },
+        ["home-manager"] = {
+          expr = vim.fn.expand(
+            '(builtins.getFlake ("git+file://$HOME/dotfiles")).nixosConfigurations.$HOST.options.home-manager.users.type.getSubOptions'
+          ) .. " []",
+        },
+      },
     },
   },
 }
