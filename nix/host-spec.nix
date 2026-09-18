de3e94997b08736ad5ab4df7cfe4ecf415cd01f7
Ricky Lopez <31072564+rickyelopez@users.noreply.github.com>
2026-09-18 11:40:26 -0700

    nix: update flake inputs

--- nix/host-spec.nix
+++ nix/host-spec.nix
@@ -54,7 +54,7 @@
             let
               user = config.hostSpec.username;
             in
-            if pkgs.stdenv.isLinux then "/home/${user}" else "/Users/${user}";
+            if pkgs.stdenv.hostPlatform.isLinux then "/home/${user}" else "/Users/${user}";
         };
 
         gpu = lib.mkOption {
